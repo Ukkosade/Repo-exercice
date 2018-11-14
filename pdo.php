514a99a9ad307e5ccffc90b2a637f3a37b4be2f3
j_val <valenza.jeremy@gmail.com>
2018-11-14 14:19:15 +0100

    lève try/catch de pdo

--- pdo.php
+++ pdo.php
@@ -9,14 +9,7 @@
  */
 
 
-try
-{
-    $pdo = new PDO($dsn, $user, $password, [
-        PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ,
-        PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8",
-        PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
-}
-catch ( PDOException $e)
-{
-    exit ("Erreur connexion BDD");
-}
+$pdo = new PDO($dsn, $user, $password, [
+	PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ,
+	PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8",
+	PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
